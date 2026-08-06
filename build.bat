7f6521253923a593aa845d9c065cee57d4e37005
מייבין במקצת <y15761576@gmail.com>
2026-08-06 17:33:28 +0300

    Update build.bat

--- build.bat
+++ build.bat
@@ -14,7 +14,7 @@ echo   2  תיקיית standalone (EXE קטן + קבצי משנה)
 echo.
 set /p CHOICE=הכנס 1 או 2: 
 
-pip install nuitka zstandard ordered-set PyQt6 bleak sounddevice vosk --quiet
+pip install nuitka zstandard ordered-set PyQt6 bleak sounddevice vosk winsdk --quiet
 
 if "%CHOICE%"=="1" goto :onefile
 if "%CHOICE%"=="2" goto :onedir
@@ -28,7 +28,7 @@ python -m nuitka ^
   --windows-icon-from-ico=app\resources\icon.ico ^
   --output-filename=SmartKosherPhone.exe ^
   --output-dir=dist ^
-  --include-package=app --include-package=bleak --include-package=vosk ^
+  --include-package=app --include-package=bleak --include-package=vosk --include-package=winsdk ^
   --include-data-dir=app\resources=app\resources ^
   --include-data-files=app\style_light.qss=app\style_light.qss ^
   --include-data-files=app\style_dark.qss=app\style_dark.qss ^
@@ -49,7 +49,7 @@ python -m nuitka ^
   --windows-icon-from-ico=app\resources\icon.ico ^
   --output-filename=SmartKosherPhone.exe ^
   --output-dir=dist_dir ^
-  --include-package=app --include-package=bleak --include-package=vosk ^
+  --include-package=app --include-package=bleak --include-package=vosk --include-package=winsdk ^
   --include-data-dir=app\resources=app\resources ^
   --include-data-files=app\style_light.qss=app\style_light.qss ^
   --include-data-files=app\style_dark.qss=app\style_dark.qss ^
@@ -64,7 +64,7 @@ pip install pyinstaller --quiet
 pyinstaller --onefile --windowed --name SmartKosherPhone --distpath dist ^
   --add-data "app\style_light.qss;app" --add-data "app\style_dark.qss;app" ^
   --add-data "app\resources;app\resources" ^
-  --hidden-import bleak --hidden-import bleak.backends.winrt ^
+  --hidden-import bleak --hidden-import bleak.backends.winrt --hidden-import winsdk ^
   --hidden-import vosk --hidden-import sounddevice ^
   --hidden-import PyQt6.QtMultimedia main.py
 goto :done
@@ -75,7 +75,7 @@ pip install pyinstaller --quiet
 pyinstaller --onedir --windowed --name SmartKosherPhone --distpath dist_dir ^
   --add-data "app\style_light.qss;app" --add-data "app\style_dark.qss;app" ^
   --add-data "app\resources;app\resources" ^
-  --hidden-import bleak --hidden-import bleak.backends.winrt ^
+  --hidden-import bleak --hidden-import bleak.backends.winrt --hidden-import winsdk ^
   --hidden-import vosk --hidden-import sounddevice ^
   --hidden-import PyQt6.QtMultimedia main.py
 
